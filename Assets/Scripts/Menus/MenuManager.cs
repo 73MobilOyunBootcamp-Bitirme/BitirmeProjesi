b1438b1d11e98318be6bb5313b0c262a9498e76a
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-01-06 21:10:10 +0300

    Menu UI Eklendi

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OnPauseMenuClick()
+    {
+        Time.timeScale = 1;
+        GoToMenu(MenuName.Pause);
+    }
+    public static void GoToMenu(MenuName name)
+    {
+        switch (name)
+        {
+            case MenuName.Main:
+                //go to MainMenu scene
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case MenuName.Pause:
+                //instantiate prefab
+                Object.Instantiate(Resources.Load("PauseMenu"));
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+    public void OnResumeButtonClick()
+    {
+        Time.timeScale = 1;
+        Destroy(gameObject);
+    }
+    public void OnQuitButtonClick()
+    {
+        Time.timeScale = 1;
+        Destroy(gameObject);
+        MenuManager.GoToMenu(MenuName.Main);
+    }
+}
